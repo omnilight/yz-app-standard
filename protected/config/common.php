5d3cb91fd2a4576ee7599b5256431ecddcf69243
Pavel Agalecky <pavel.agalecky@gmail.com>
2013-10-29 17:42:12 +0400

    Initial prepare

--- protected/config/common.php
+++ protected/config/common.php
@@ -4,21 +4,21 @@
  */
 
 // Defining aliases
-Yii::setPathOfAlias('vendor',YZ_VENDOR_DIR);
+Yii::setPathOfAlias('vendor', YZ_VENDOR_DIR);
 
 return array(
-    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'YzEngine Standard Application',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'YzEngine Standard Application',
 
 //    'theme' => 'theme',
 
     'language' => 'ru',
 
     // preloading 'log' component
-    'preload'=>array('log','yz'),
+    'preload' => array('log', 'yz'),
 
     // autoloading model and component classes
-    'import'=>array(
+    'import' => array(
         'application.models.*',
         'application.components.*',
     ),
@@ -29,13 +29,55 @@ return array(
         'yz' => array(
             'class' => 'vendor.omnilight.yz.Yz',
         ),
-        'settings'=>array(
-            'class'=>'yz.components.YzSettingsComponent',
+        'settings' => array(
+            'class' => 'yz.components.YzSettingsComponent',
         ),
-        'db'=>CMap::mergeArray(
-            require('db.php'),
-            array()
+        'db' => CMap::mergeArray(
+                require('db.php'),
+                array()
+            ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                array(
+                    'class'=>'CWebLogRoute',
+                ),
+                */
+            ),
         ),
+        'cache' => defined('DEV_ENV') ?
+                array(
+                    'class' => 'system.caching.CDummyCache',
+                    'behaviors' => array(
+                        'Tags' => array(
+                            'class' => 'yz.behaviors.YzCacheTagsBehavior',
+                        )
+                    )
+                ) :
+                array(
+                    'class' => 'system.caching.CDummyCache',
+                    /*'servers' => array(
+                        array(
+                            'host' => 'localhost',
+                            'port' => '11211',
+                        ),
+                    ),*/
+                    'behaviors' => array(
+                        'Tags' => array(
+                            'class' => 'yz.behaviors.YzCacheTagsBehavior',
+                        )
+                    )
+                ),
+
+    ),
+
+    'params' => array(
 
     ),
 );
---
+++ web/backend.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+// Checking for development mode
+if(is_file(dirname(__FILE__).'/.devenv'))
+    define('DEV_ENV',true);
+
+define('YZ_ROOT',dirname(__FILE__));
+define('YZ_APP_DIR',YZ_ROOT.'/protected');
+define('YZ_VENDOR_DIR',YZ_ROOT.'/protected/vendor');
+
+// change the following paths if necessary
+$yii=YZ_VENDOR_DIR.'/yiisoft/yii/framework/yii.php';
+$config=YZ_APP_DIR.'/config/backend.php';
+
+// remove the following lines when in production mode
+defined('DEV_ENV') and (defined('YII_DEBUG') or define('YII_DEBUG',true));
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
